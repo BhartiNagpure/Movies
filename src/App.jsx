e2bc7b85756cef3b8ba1cac2ffa5f020fa13fe11
Bharti Vilas Nagpure <104631882+BhartiNagpure@users.noreply.github.com>
2023-07-20 14:40:09 +0530

    notfound page added

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,7 @@ import Home from './pages/home/Home';
 import Details from './pages/details/Details';
 import Explore from './pages/explore/Explore';
 import SearchResult from './pages/searchResult/SearchResult';
-import PageNotFound from './pages/404/pageNotFound';
-
+import NotFound from "./pages/404/NotFound"
 
 function App() {
   const dispatch = useDispatch()
@@ -65,7 +64,7 @@ function App() {
         <Route path="/:mediaType/:id" element={<Details />} />
         <Route path="/search/:query" element={<SearchResult />} />
         <Route path="/explore/:mediaType" element={<Explore />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </BrowserRouter>
